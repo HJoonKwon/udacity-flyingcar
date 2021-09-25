5ad1cc5639d6c1d929318023fb8991edef861438
Joon <dsazz801@gmail.com>
2021-09-25 22:04:11 +0900

    add path pruning functions

--- planning/planning.py
+++ planning/planning.py
@@ -2,6 +2,31 @@ from enum import Enum
 from queue import PriorityQueue
 import numpy as np 
 
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+def collinearity_check(p1, p2, p3, epsilon=1e-6):   
+    m = np.concatenate((p1, p2, p3), 0)
+    det = np.linalg.det(m)
+    return abs(det) < epsilon
+
+def prune_path(path):
+    if path is not None:
+        pruned_path = [p for p in path]
+        # TODO: prune the path!
+        i = 0 
+        while i < len(pruned_path) - 2:
+            p1 = point(pruned_path[i])
+            p2 = point(pruned_path[i+1])
+            p3 = point(pruned_path[i+2]) 
+            if collinearity_check(p1, p2, p3):
+                pruned_path.remove(pruned_path[i+1])
+            else:
+                i += 1             
+    else:
+        pruned_path = path
+        
+    return pruned_path
 
 class Action(Enum):
     """
